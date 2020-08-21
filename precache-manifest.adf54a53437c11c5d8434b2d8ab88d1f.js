f4aaa76227b6749c31d11922dd2914dc066047cb
nowlejeong <iljin10637@naver.com>
2020-08-22 00:17:49 +0900

    Updates

---
+++ precache-manifest.adf54a53437c11c5d8434b2d8ab88d1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb063dcd27c82da7232e9b1893f96cd",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4fb2ab6fda6b93cd2132",
+    "url": "/movie_app/static/css/main.672a5149.chunk.css"
+  },
+  {
+    "revision": "48d04c1484dc82f93f2a",
+    "url": "/movie_app/static/js/2.77275e5f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.77275e5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb2ab6fda6b93cd2132",
+    "url": "/movie_app/static/js/main.6b8f678e.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
